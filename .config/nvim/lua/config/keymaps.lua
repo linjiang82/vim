adb2cc063d20b40dd3cade9bde90a1a096433f7e
Jiang Lin <jiang.lin@ladbrokes.com.au>
2024-09-11 18:51:26 +1000

    chore: conditionally use prettier formatter

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,7 +4,6 @@
 --
 vim.keymap.set("i", "jj", "<esc>", { noremap = true, silent = true, desc = "<ESC>" })
 -- preferrablly to map cmd to ctrl key, set the hex code 0x13 in iterm
-vim.keymap.set("n", "<C-s>", "<esc>:w<CR>", { noremap = true, silent = true, desc = "Save File" })
 
 -- NvimTmuxNavigate
 vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { silent = true })
@@ -15,5 +14,5 @@ vim.keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>", { silent = 
 vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", { silent = true })
 
 -- Buffers
-vim.keymap.set("n", "<Tab>", ":bnext<CR>", { silent = true, desc = "Next Buffer" })
+-- cannot map Tab here as it will override what <C-i> does
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { silent = true, desc = "Previous Buffer" })
